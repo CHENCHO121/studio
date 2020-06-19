b78de5efc49323f6c7c8d29e214c50cf29fea2d5
chencho121 <chenchogyeltshen000@gmail.com>
2020-06-19 18:52:14 +0530

    debug

--- project/settings.py
+++ project/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd5f=a)ypswr7k3_o%k&#fs1_sfg@&i1sf@i7d4)n$k297qqf6!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '.herokuapp.com']
 
 
 # Application definition
