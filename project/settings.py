95ec709829d5b0f03eee1456c072100bff54ce3d
chencho121 <chenchogyeltshen000@gmail.com>
2020-06-22 19:03:56 +0530

    static commit

--- project/settings.py
+++ project/settings.py
@@ -77,21 +77,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
